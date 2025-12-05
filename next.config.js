10a4fcc8742e7f6b2dc6d6cefb9c8214b0b5ea79
Tomoya185-miyawaki <tomo16185@gmail.com>
2025-12-05 16:17:45 +0900

    設定ファイルを修正

--- next.config.js
+++ next.config.js
@@ -6,6 +6,13 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig
